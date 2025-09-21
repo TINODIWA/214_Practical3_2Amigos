9b9605956694cb296926a82c3c379816fa72aeb9
TINODIWA <nathaniel.chisadza@gmail.com>
2025-09-21 22:32:38 +0200

    Skeleton of ChatRoom class

---
+++ ChatRoom.h
@@ -0,0 +1,25 @@
+#ifndef CHATROOM_H
+#define CHATROOM_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class ChatRoom {
+    public:
+        ChatRoom();
+        ~ChatRoom();
+
+        virtual void registerUser(Users user) = 0;
+        void sendMessage(string message, Users fromUser);
+        void saveMessage(string message, Users fromUser);
+        virtual void removeUser(Users user) = 0;
+        virtual Iterator* createIterator() = 0;
+
+    private:
+        vector<Users> users;
+        vector<string> chatHistory;
+};
+
+#endif
